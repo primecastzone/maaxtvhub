a0e35779c92eaab112bae3f6698d5104aa278601
primecastzone <vinitasukheja1@gmail.com>
2025-07-16 15:14:30 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'Disneyplus TV'
+project = 'hbomax tvsignin'
 
 
 # The full version, including alpha/beta/rc tags
